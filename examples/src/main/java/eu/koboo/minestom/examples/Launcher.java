9f7f51276969892efa2a6ad07ac19beec3f7e03a
Koboo <admin@koboo.eu>
2025-02-08 12:16:18 +0100

    Update Launcher to show initialization of ViewRegistry

--- examples/src/main/java/eu/koboo/minestom/examples/Launcher.java
+++ examples/src/main/java/eu/koboo/minestom/examples/Launcher.java
@@ -1,6 +1,13 @@
 package eu.koboo.minestom.examples;
 
+import dev.rollczi.litecommands.LiteCommands;
+import dev.rollczi.litecommands.minestom.LiteMinestomFactory;
+import eu.koboo.minestom.api.ViewRegistry;
+import eu.koboo.minestom.core.MinestomInvue;
+import eu.koboo.minestom.examples.command.CommandView;
+import eu.koboo.minestom.examples.command.ExampleMultiViewCommand;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.CommandSender;
 import net.minestom.server.extras.MojangAuth;
 
 public class Launcher {
@@ -9,5 +16,25 @@ public class Launcher {
         MinecraftServer minecraftServer = MinecraftServer.init();
         MojangAuth.init();
         minecraftServer.start("127.0.0.1", 25565);
+
+        // Create a new instance of the ViewRegistry.
+        ViewRegistry viewRegistry = MinestomInvue.create();
+
+        // Register the listener of invue
+        viewRegistry.enable();
+
+        // If ur server shuts down, you can disable view registries by:
+        //viewRegistry.disable();
+
+        LiteCommands<CommandSender> liteCommands = LiteMinestomFactory.builder()
+            .commands(
+                new CommandView(viewRegistry),
+                new ExampleMultiViewCommand(viewRegistry)
+            )
+            .settings(configurator ->
+                configurator.permissionResolver((sender, permission) -> true)
+            )
+            .build();
+        liteCommands.register();
     }
 }
